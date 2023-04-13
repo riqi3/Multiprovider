a3643d2df956f3ee2394e5a6b1b44a2675275210
riqi3 <enrique.pacudan@usjr.edu.ph>
2023-04-13 11:12:43 +0800

    finsihed

--- multiprovider/lib/home.dart
+++ multiprovider/lib/home.dart
@@ -1,6 +1,51 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-String now() => DateTime.now().toIso8601String(); 
+String now() => DateTime.now().toIso8601String();
+
+@immutable
+class Seconds {
+  final String value;
+  Seconds() : value = now();
+}
+
+@immutable
+class Minutes {
+  final String value;
+  Minutes() : value = now();
+}
+
+class SecondsWidget extends StatelessWidget {
+  const SecondsWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final seconds = context.watch<Seconds>();
+    return Expanded(
+      child: Container(
+        color: Colors.amber,
+        height: 100,
+        child: Text(seconds.value),
+      ),
+    );
+  }
+}
+
+class MinutesWidget extends StatelessWidget {
+  const MinutesWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final minutes = context.watch<Minutes>();
+    return Expanded(
+      child: Container(
+        color: Colors.deepOrange,
+        height: 100,
+        child: Text(minutes.value),
+      ),
+    );
+  }
+}
 
 class home extends StatelessWidget {
   const home({super.key});
@@ -11,6 +56,34 @@ class home extends StatelessWidget {
       appBar: AppBar(
         title: Text('home'),
       ),
+      body: MultiProvider(
+        providers: [
+          StreamProvider.value(
+            value: Stream<Seconds>.periodic(
+              const Duration(seconds: 1),
+              (_) => Seconds(),
+            ),
+            initialData: Seconds(),
+          ),
+          StreamProvider.value(
+            value: Stream<Minutes>.periodic(
+              const Duration(minutes: 1),
+              (_) => Minutes(),
+            ),
+            initialData: Minutes(),
+          ),
+        ],
+        child: Column(
+          children: [
+            Row(
+              children: const [
+                SecondsWidget(),
+                MinutesWidget(),
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
-} 
+}
